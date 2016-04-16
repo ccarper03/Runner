b934e069412d1a18ab787c36f3b1471227b53ca9
ccarper03 <ccarper03@gmail.com>
2016-04-16 00:33:19 -0400

    Added a game over menu and a restart option

---
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFunctions : MonoBehaviour 
+{
+	public void BtnPlayClick()
+	{
+		Application.LoadLevel("Main");
+	}
+//	public void BtnOptionsClick()
+//	{
+//		gameObject.SetActive(false);
+//	}
+//	public void BtnCreditsClick()
+//	{
+//		Application.LoadLevel("GetButton and GetKey");
+//
+//	}
+	public void BtnExitClick()
+	{
+		//Application.Quit ();
+		Debug.Log("You have Quit the program");
+	}
+//	public void BTN_Ctr()
+//	{
+//		Debug.Log("Controls");
+//	}
+//	public void BTN_SFX()
+//	{
+//		Debug.Log("Sound Effects");
+//	}
+//	public void BTN_ScM()
+//	{
+//		Debug.Log("Social Media");
+//	}
+//	public void BTN_Bck()
+//	{
+//		Debug.Log("Back");
+//	}
+//	public void BTN_Msc()
+//	{
+//		Debug.Log("Music");
+//	}
+}
