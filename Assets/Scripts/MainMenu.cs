406d17e225a06d2a18d64e408dfea59f7461229f
ccarper03 <ccarper03@gmail.com>
2016-04-20 11:56:43 -0400

    Created a Main Menu , and a lose state.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+	public void start()
+	{
+		SceneManager.LoadScene("Main");
+	}
+	public void Options()
+	{
+		SceneManager.LoadScene("Options");
+	}
+	public void Credits()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+
+	public void Mainmenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void Gameover()
+	{
+		SceneManager.LoadScene("GameOver");
+	}
+
+	public void Exit()
+	{
+		Debug.Log ("End of Program");
+	}
+
+}
+// SceneManager.LoadScene("GameOver");
