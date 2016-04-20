117280d7a84f692407cddd9e45aacdc9ccaf6147
ccarper03 <ccarper03@gmail.com>
2016-04-20 10:38:27 -0400

    Added a game over screen and options to start over or quit.

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour 
 {
@@ -24,7 +25,7 @@ public class GameControl : MonoBehaviour
 	{
 		if(isGameOver)
 		{
-			return;
+			SceneManager.LoadScene("GameOver");;
 		}
 
 		totalTimeElapsed += Time.deltaTime;
